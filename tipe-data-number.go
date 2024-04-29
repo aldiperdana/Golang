4f050ccaa54209a4abf2e9b3516e79a810de3b65
Muhammad Aldi <muhammad.aldi.perdana.putra@gmail.com>
2024-04-29 19:02:02 +0800

    Belajar Golang Hari Pertama

---
+++ tipe-data-number.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var angka int = 30
+	var (
+		angka1 byte  = 1
+		angka2 int16 = 2
+		angka3 int32 = 3
+		angka4 int64 = 4
+	)
+	fmt.Println(angka)
+	fmt.Println(angka1)
+	fmt.Println(angka2)
+	fmt.Println(angka3)
+	fmt.Println(angka4)
+}
---
+++ tipe-data-string.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	nama := "Muhammad Aldi Perdana Putra"
+	fmt.Println("Nama saya adalah : ", nama)
+}
